50fbeabbee1bb70ef4365233a0952eb0a749b6d7
Onlyprogrammer <Ruskybear@yandex.ru>
2017-11-13 22:33:27 +0300

    Add files via upload

---
+++ scripts/main_buttons.jq.js
@@ -0,0 +1,20 @@
+var open = false;
+
+$("head style").append('.hor_menu{margin: 0px;padding: 0px;width:50px;list-style: none;display: flex;flex-direction: column;justify-content:center;}');
+$("head style").append('.hor_div{background-color:#a33500;width:150px;color: white;display: flex;align-items: center;font-family: \'Roboto\', sans-serif;font-size: 18px;}');
+$("head style").append('.hor_div ul{list-style:none;padding:0px;margin-left:5px;}');
+$("head style").append('.hor_div ul li{margin:8px;padding:8px;background-color:#c24306;cursor: pointer;border-radius:5px;}');
+
+
+$("#artwork").on("click",function(){
+  if (open != true) {
+  $("body").append('<div class="hor_div bighp"><ul class=".hor_menu"><li>История создания</li><li>Персонажи</li></ul></div>');
+  $("head").append('<style id="clr">.bighp {position:absolute;top:60px;left:125px;}</style>');
+  open = true;
+  }
+  else {
+  $(".hor_div").remove();
+  $("head #clr").remove();
+  open = false;
+  }
+});
